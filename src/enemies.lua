90bb57ae48a6fed6a9824e8a1a70806d72a85366
Lucas <lucas.hardman92@gmail.com>
2017-05-21 14:10:38 -0300

    pokemon.png without background

--- src/enemies.lua
+++ src/enemies.lua
@@ -9,48 +9,162 @@ local function loadEnemies ()
 	    frames =
 	    {
 	    
-	        {   -- frame 1
-	            x = 80,
-	            y = 25,
+	        {   -- lopunny walking down 1
+	            x = 80 + 0 * 27,
+	            y = 25 + 0 * 27,
 	            width = 27,
 	            height = 27
 	        },
 
-		    {   -- frame 2
-		        x = 80 + 27,
-		        y = 25,
+		    {   -- lopunny walking down 2
+		        x = 80 + 1 * 27,
+		        y = 25 + 0 * 27,
 		        width = 27,
 	            height = 27
 		    },
 
-		    {   -- frame 3
+		    {   -- lopunny walking down 3
 		        x = 80 + 2 * 27,
-		        y = 25,
+		        y = 25 + 0 * 27,
 		        width = 27,
 	            height = 27
 		    },
 
-		    {   -- frame 4
+		    {   -- lopunny walking down 4
 		        x = 80 + 3 * 27,
-		        y = 25,
+		        y = 25 + 0 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking left 1
+		        x = 80 + 0 * 27,
+		        y = 25 + 1 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking left 2
+		        x = 80 + 1 * 27,
+		        y = 25 + 1 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking left 3
+		        x = 80 + 2 * 27,
+		        y = 25 + 1 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking left 4
+		        x = 80 + 3 * 27,
+		        y = 25 + 1 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking right 1
+		        x = 80 + 0 * 27,
+		        y = 25 + 2 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking right 2
+		        x = 80 + 1 * 27,
+		        y = 25 + 2 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking right 3
+		        x = 80 + 2 * 27,
+		        y = 25 + 2 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking right 4
+		        x = 80 + 3 * 27,
+		        y = 25 + 2 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking up 1
+		        x = 80 + 0 * 27,
+		        y = 25 + 3 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking up 2
+		        x = 80 + 1 * 27,
+		        y = 25 + 3 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking up 3
+		        x = 80 + 2 * 27,
+		        y = 25 + 3 * 27,
+		        width = 27,
+	            height = 27
+		    },
+
+		    {   -- lopunny walking up 4
+		        x = 80 + 3 * 27,
+		        y = 25 + 3 * 27,
 		        width = 27,
 	            height = 27
 		    }
 	    }
 	}
-	local ratata = graphics.newImageSheet( "images/monsters/pokemon.png", sheetPokemon )
+	local pokemon = graphics.newImageSheet( "images/monsters/pokemon.png", sheetPokemon )
 
 	-- walking down sequences table
-	local sequences_skin1 = {
+	local sequences_lopunnyDown = {
 	    -- consecutive frames sequence
 	    {
-	        name = "skin1",
-	        frames = { 1,2,3 },
+	        name = "lopunnyDown",
+	        frames = { 1,2,3,4 },
 	        time = 400
 	    }
 	}
 
-	local animationSkin1 = display.newSprite(ratata, sequences_skin1)
+	-- walking left sequences table
+	local sequences_lopunnyLeft = {
+	    -- consecutive frames sequence
+	    {
+	        name = "lopunnyLeft",
+	        frames = { 5,6,7,8 },
+	        time = 400
+	    }
+	}
+
+	-- walking right sequences table
+	local sequences_lopunnyRight = {
+	    -- consecutive frames sequence
+	    {
+	        name = "lopunnyRight",
+	        frames = { 9,10,11,12 },
+	        time = 400
+	    }
+	}
+
+	-- walking up sequences table
+	local sequences_lopunnyUp = {
+	    -- consecutive frames sequence
+	    {
+	        name = "lopunnyUp",
+	        frames = { 13,14,15,16 },
+	        time = 400
+	    }
+	}
+
+	local animationSkin1 = display.newSprite(pokemon, sequences_lopunnyDown)
 	animationSkin1.height = 64
 	animationSkin1.width = 64
 	animationSkin1.x = 150
